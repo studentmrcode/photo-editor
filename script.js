8bcbcec03e96b8c4bfcfff6c92f381931960d93d
David <davidk@mrcodeswildride.com>
2018-11-15 20:05:34 +0000

    init

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-// Make variables for the card, title paragraph, buttons, text box, color picker, and slider.
+// Make variables for the sliders.
 // You can name them anything you want, but for simplicity, name them the same as the IDs.
 
 
-// Add event listeners to the buttons, text box, color picker, and slider.
+// Add event listeners to the sliders.
 // You can name the functions anything you want, but pick something that indicates action.
 
 
-// Make functions to change the card's color, card's title, title's color, and title's font size.
+// Make functions to change the image's width, opacity, and rotation.
