338730f46d30a1b3b61f2efc05dbcd2fbc44ac27
David <davidk@mrcodeswildride.com>
2018-11-15 19:32:22 +0000

    init

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-// Make variables for the text box, button, and paragraph.
+// Make variables for the card, title paragraph, buttons, text box, color picker, and slider.
 // You can name them anything you want, but for simplicity, name them the same as the IDs.
 
 
-// Add an event listener to the button to show the story.
-// You can name the function anything you want, but pick something that indicates action, like showStory.
+// Add event listeners to the buttons, text box, color picker, and slider.
+// You can name the functions anything you want, but pick something that indicates action.
 
 
-// Make a function to show the story.
+// Make functions to change the card's color, card's title, title's color, and title's font size.
