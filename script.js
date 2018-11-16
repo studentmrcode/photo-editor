793d7e4516b10491d17f4df0f97a3f0545ef6185
David <davidk@mrcodeswildride.com>
2018-11-16 20:56:06 +0000

    added Section text to script

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-// Make variables for the sliders.
+// Section 1: Make separate variables for the three sliders.
 // You can name them anything you want, but for simplicity, name them the same as the IDs.
 
 
-// Add event listeners to the sliders.
+// Section 2: Add separate event listeners to the three sliders.
 // You can name the functions anything you want, but pick something that indicates action.
 
 
-// Make functions to change the image's width, opacity, and rotation.
+// Section 3: Make separate functions to change the image's width, opacity, and rotation.
