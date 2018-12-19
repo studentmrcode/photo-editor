fccc35f8865a82f1745edf267a4c853dd16e9597
David <davidk@mrcodeswildride.com>
2018-12-19 23:58:05 +0000

    removed comments from JS

--- script.js
+++
@@ -1,9 +0,0 @@
-// Section 1: Make separate variables for the image and three sliders.
-// You can name them anything you want, but for simplicity, name them the same as the IDs.
-
-
-// Section 2: Add separate event listeners to the three sliders.
-// You can name the functions anything you want, but pick something that indicates action.
-
-
-// Section 3: Make separate functions to change the image's width, opacity, and rotation.
